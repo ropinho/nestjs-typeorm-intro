b23aa239065f7db3d03d80bff889cb0937b0d914
Ronaldd Pinho <ronaldd.pinho@lambda3.com.br>
2021-09-03 22:56:03 -0300

    Remove as classe de App exceto AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
